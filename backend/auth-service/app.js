723af22e3af53117d680311a65233fdae53c8338
Sarthak sharma <sharma.sarthak222872@gmail.com>
2025-02-06 09:45:35 +0530

    set up routes

--- backend/auth-service/app.js
+++ backend/auth-service/app.js
@@ -2,12 +2,15 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+const userRouter = require("./routes/userRoute")
 
 const PORT = process.env.PORT; 
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
+
+app.use("/", userRouter)
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
