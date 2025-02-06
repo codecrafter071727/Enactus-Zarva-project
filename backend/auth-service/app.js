2f8be2d51a46fb2975d6af7f6969606926d43ca4
Sarthak sharma <sharma.sarthak222872@gmail.com>
2025-02-06 08:49:31 +0530

    corrected the folder structure

---
+++ backend/auth-service/app.js
@@ -0,0 +1,14 @@
+require('dotenv').config(); 
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT; 
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
